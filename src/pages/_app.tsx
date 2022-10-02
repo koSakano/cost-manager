1815a4f3a7366b7b1d9ee708528db0d9928e9987
koSakano <koki.sakano@sencorp.co.jp>
2022-10-02 17:08:47 +0900

    theme provider登録

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
